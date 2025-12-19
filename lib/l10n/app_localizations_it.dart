6f40b0494177657a50870f0964e39418d8b654ef
zemanux <emmanuel.lefebvre@gmail.com>
2025-12-19 13:45:17 +0100

    chore: upgrade dependencies

---
+++ lib/l10n/app_localizations_it.dart
@@ -0,0 +1,13 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Italian (`it`).
+class AppLocalizationsIt extends AppLocalizations {
+  AppLocalizationsIt([String locale = 'it']) : super(locale);
+
+  @override
+  String get add_to => 'Aggiungi a';
+}
