3c08724f3aca48d404ced1195e4c58a002d78973
zemanux <emmanuel.lefebvre@gmail.com>
2025-12-19 13:45:17 +0100

    chore: upgrade dependencies

---
+++ lib/l10n/app_localizations_nl.dart
@@ -0,0 +1,13 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Dutch Flemish (`nl`).
+class AppLocalizationsNl extends AppLocalizations {
+  AppLocalizationsNl([String locale = 'nl']) : super(locale);
+
+  @override
+  String get add_to => 'Toevoegen aan';
+}
